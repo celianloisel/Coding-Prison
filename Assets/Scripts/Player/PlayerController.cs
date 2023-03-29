a878a52913ed39957ec94fa7e7173d8835df57c8
celianloisel <celianloisel@gmail.com>
2023-03-29 09:14:50 +0200

    feature/add-movement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigBod;
+    
+    [SerializeField]
+    private float m_MoveSpeed;
+    private Vector2 _movement;
+
+    void Start()
+    {
+        _rigBod = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        _movement.x = Input.GetAxis("Horizontal");
+        _movement.y = Input.GetAxis("Vertical");
+        _rigBod.MovePosition(_rigBod.position + _movement * m_MoveSpeed * Time.fixedDeltaTime);
+    }
+}
