5e5e54f1843335962283b70d5869bdea09ce512d
celianloisel <celianloisel@gmail.com>
2023-03-31 11:47:46 +0200

    finv1

--- Assets/Scripts/Bot/Spot.cs
+++ Assets/Scripts/Bot/Spot.cs
@@ -4,11 +4,17 @@ namespace Bot
 {
     public class Spot : MonoBehaviour
     {
+        private Timer timer;
+
+        void Start()
+        {
+            timer = FindObjectOfType<Timer>();
+        }
         void OnTriggerEnter2D(Collider2D other)
         {
             if (other.CompareTag("Player"))
             {
-                Application.Quit();
+                Destroy(timer.gameObject);
             }
         }
     }
---
+++ Assets/Scripts/Game/Timer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    private float startTime;
+    private float elapsedTime;
+    private bool timerRunning;
+    public string levelToLoad;
+
+    void Start()
+    {
+        startTime = Time.time;
+        elapsedTime = 0f;
+        timerRunning = true;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Update()
+    {
+        if(timerRunning)
+        {
+            elapsedTime = Time.time - startTime;
+            float remainingTime = 10 * 60 - elapsedTime;
+            Debug.Log("Temps restant : " + remainingTime.ToString("F2") + " secondes");
+
+            if(remainingTime <= 0f)
+            {
+                Debug.Log("Temps écoulé !");
+                SceneManager.LoadScene(levelToLoad);
+                timerRunning = false;
+            }
+        }
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        startTime = Time.time - elapsedTime;
+    }
+}
