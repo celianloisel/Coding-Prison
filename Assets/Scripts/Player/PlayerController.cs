36cb71ae10a37da6a6af0a4b9e4366b74748fe14
Kisat0 <ghanembenyoucef@gmail.com>
2023-03-31 10:01:10 +0200

    add sprite renderer

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D _rigBod;
     public Animator animator;
+    public SpriteRenderer spriteRenderer;
     
     [SerializeField]
     private float m_MoveSpeed;
@@ -23,9 +25,25 @@ public class PlayerController : MonoBehaviour
         _movement.y = Input.GetAxis("Vertical");
         _rigBod.MovePosition(_rigBod.position + _movement * m_MoveSpeed * Time.fixedDeltaTime);
 
+        Flip(_movement.x);
+
         float characterVelocity = Mathf.Abs(_movement.magnitude);
         animator.SetFloat("Speed", characterVelocity);
 
+
+    }
+
+    void Flip(float _Velocity)
+    {
+        if(_Velocity < 0.1f)
+        {
+            spriteRenderer.flipX = true;
+
+        }else if (_Velocity > 0.1f) 
+
+        {
+            spriteRenderer.flipX=false; 
+        }
 
     }
 }
