8f8d0ebc01901913d710a62af763f65ce3553caa
celianloisel <celianloisel@gmail.com>
2023-03-31 11:16:43 +0200

    finv1

---
+++ Assets/Scripts/Game/End.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class End : MonoBehaviour
+{
+    public bool porte = false;
+    public string sceneToLoad;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            porte = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && porte)
+        {
+            if (GameObject.Find("Player").GetComponent<Inventory>().c.Equals(true))
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            porte = false;
+        }
+    }
+}
