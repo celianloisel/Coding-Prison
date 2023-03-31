2bac240c6eb0b944e7e7e320160e7dac5ab5e980
celianloisel <celianloisel@gmail.com>
2023-03-31 10:09:08 +0200

    vite

---
+++ Assets/Scripts/Bot/Spot.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Bot
+{
+    public class Spot : MonoBehaviour
+    {
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
